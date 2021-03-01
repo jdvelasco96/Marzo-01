db641954fbee0b37358290ee7a15903c9d231a57
jdvelasco96 <78481663+jdvelasco96@users.noreply.github.com>
2021-03-01 08:59:25 +0100

    rama1 (#1)
    Co-authored-by: Ceinmark <Ceinmark@DESKTOP-AURVFUI>

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -3,8 +3,8 @@ package prueba;
 public class Prueba {
 
 	public static void main(String[] args) {
-		System.out.println("rama maestra");
-
+		System.out.println("rama maestra");		
+		System.out.println("rama1");
 	}
 
 }
