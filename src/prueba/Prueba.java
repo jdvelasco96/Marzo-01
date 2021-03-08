036d0e5a74f906f2e030bb43690a44174251268a
jdvelasco96 <78481663+jdvelasco96@users.noreply.github.com>
2021-03-08 08:51:46 +0100

    Cambios rama1 (#2)
    Co-authored-by: Ceinmark <Ceinmark@DESKTOP-AURVFUI>

--- src/prueba/Prueba.java
+++ src/prueba/Prueba.java
@@ -4,9 +4,15 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		
-		int num1=0, num2=0;
-		System.out.println("SUMAR");
+		int num1=5, num2=10;
+		System.out.println("SUMAR "+ num1 + " " + num2);
 		
+		sumar(num1,num2);
+		
+	}
+	
+	public static void sumar(int num1,int num2) {
+		System.out.println(num1+num2);
 	}
 
 }
